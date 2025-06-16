73b1277888cc6ae9abdafdb80cda6677fd2d7763
12300238 <prades.mathew@gmail.com>
2025-06-16 23:05:15 +0200

    fermeture de la nav quand on click dessu

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
-  document.querySelector('.menu-toggle').addEventListener('click', () => {
+document.querySelector('.menu-toggle').addEventListener('click', () => {
+  document.getElementById('navbar').classList.toggle('open');
+});
+
+document.getElementById('navbar').addEventListener('click', () => {
     document.getElementById('navbar').classList.toggle('open');
-  });
+});
